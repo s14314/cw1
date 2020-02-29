9e1198d5ba77c6a389928f55b74c22d309d6832f
s14314 <s14314@pjwstk.edu.pl>
2020-02-29 17:47:01 +0100

    commit2

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if(result.IsSuccessStatusCode)
+            {
+                var html = await result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
